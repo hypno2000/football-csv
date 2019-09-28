fbe4d4abf1424f24f2ce46de597135707e63e9d5
lososacku <r.b.hicks@gmail.com>
2019-09-29 01:50:23 +0200

    created now file to put the protobuf schema in

---
+++ lib/derivco_football/protobuf_messages.ex
@@ -0,0 +1,23 @@
+defmodule DerivcoFootball.ProtobufMessages do
+    use Protobuf, """
+    message LeagueSeasonPairs{
+      repeated string values = 1;
+    }
+    
+    message LeagueSeasonPairResult{
+      required string date = 1;
+      required string home_team = 2;
+      required string away_team = 3;
+      required uint32 fthg = 4;
+      required uint32 ftag = 5;
+      required string ftr = 6;
+      required uint32 hthg = 7;
+      required uint32 htag = 8;
+      required string htr = 9;
+    }
+
+    message LeagueSeasonPairResults{
+      repeated LeagueSeasonPairResult leagueSeasonPairResult = 1;
+    }
+  """
+end
