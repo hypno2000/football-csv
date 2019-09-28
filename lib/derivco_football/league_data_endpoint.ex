fd3fd8f8819f2e5f2d974049499cb5280c526e8b
lososacku <r.b.hicks@gmail.com>
2019-09-28 18:25:33 +0200

    endpoint stubs in place

--- lib/derivco_football/league_data_endpoint.ex
+++ lib/derivco_football/league_data_endpoint.ex
@@ -11,13 +11,22 @@ defmodule DerivcoFootball.LeagueDataEndpoint do
   plug(:dispatch)
 
   get "/api/json/league_season_pairs" do
+    send_resp(conn, 200, "/api/json/league_season_pairs here")
+  end
 
-    IO.puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-    IO.puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-    IO.inspect conn
-    IO.puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-    IO.puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  get "/api/json/league_season_results/:league_season_pair" do
+    send_resp(conn, 200, "/api/json/league_season_results/#{league_season_pair} here")
+  end
 
-    send_resp(conn, 200, "wackadoo")
+  get "/api/protobuf/league_season_pairs" do
+    send_resp(conn, 200, "/api/protobuf/league_season_pairs here")
+  end
+
+  get "/api/protobuf/league_season_results/:league_season_pair" do
+    send_resp(conn, 200, "/api/protobuf/league_season_results/#{league_season_pair} here")
+  end
+
+  get _ do
+    send_resp(conn, 404, "they like...aren't here or something")
   end
 end
