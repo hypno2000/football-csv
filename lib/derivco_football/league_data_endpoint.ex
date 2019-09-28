675df3b2fb309e82479a4cb9371f9b36027d69d3
lososacku <r.b.hicks@gmail.com>
2019-09-28 19:15:29 +0200

    created internal handler functions

--- lib/derivco_football/league_data_endpoint.ex
+++ lib/derivco_football/league_data_endpoint.ex
@@ -10,23 +10,51 @@ defmodule DerivcoFootball.LeagueDataEndpoint do
   plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
   plug(:dispatch)
 
+  #
+  # external api
+  #
+
   get "/api/json/league_season_pairs" do
-    send_resp(conn, 200, "/api/json/league_season_pairs here")
+    handle_get_json_league_season_pairs(conn)    
   end
 
   get "/api/json/league_season_results/:league_season_pair" do
-    send_resp(conn, 200, "/api/json/league_season_results/#{league_season_pair} here")
+    handle_json_get_league_season_results(conn, league_season_pair)    
   end
 
   get "/api/protobuf/league_season_pairs" do
-    send_resp(conn, 200, "/api/protobuf/league_season_pairs here")
+    handle_protobuf_get_league_season_pairs(conn)    
   end
 
   get "/api/protobuf/league_season_results/:league_season_pair" do
-    send_resp(conn, 200, "/api/protobuf/league_season_results/#{league_season_pair} here")
+    handle_protobuf_get_league_season_results(conn, league_season_pair)    
   end
 
   get _ do
+    handle_404(conn)   
+  end
+
+  #
+  # internal handlers
+  #
+
+  defp handle_get_json_league_season_pairs(conn) do
+    send_resp(conn, 200, "/api/json/league_season_pairs here")
+  end
+
+  defp handle_json_get_league_season_results(conn, league_season_pair) do
+    send_resp(conn, 200, "/api/json/league_season_results/#{league_season_pair} here")
+  end
+
+  defp handle_protobuf_get_league_season_pairs(conn) do
+    send_resp(conn, 200, "/api/protobuf/league_season_pairs here")
+  end
+
+  defp handle_protobuf_get_league_season_results(conn, league_season_pair) do
+    send_resp(conn, 200, "/api/protobuf/league_season_results/#{league_season_pair} here")
+  end
+
+  defp handle_404(conn) do
     send_resp(conn, 404, "they like...aren't here or something")
   end
 end
