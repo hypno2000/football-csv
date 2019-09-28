2149d49308584b796334160a8b9ec5893a097bef
lososacku <r.b.hicks@gmail.com>
2019-09-29 01:51:08 +0200

    changed references to the protobuf structs to use the new file.

--- lib/derivco_football/league_data_endpoint.ex
+++ lib/derivco_football/league_data_endpoint.ex
@@ -1,27 +1,3 @@
-defmodule ProtobufMessages do
-    use Protobuf, """
-    message LeagueSeasonPairs{
-      repeated string values = 1;
-    }
-    
-    message LeagueSeasonPairResult{
-      required string date = 1;
-      required string home_team = 2;
-      required string away_team = 3;
-      required uint32 fthg = 4;
-      required uint32 ftag = 5;
-      required string ftr = 6;
-      required uint32 hthg = 7;
-      required uint32 htag = 8;
-      required string htr = 9;
-    }
-
-    message LeagueSeasonPairResults{
-      repeated LeagueSeasonPairResult leagueSeasonPairResult = 1;
-    }
-  """
-end
-
 defmodule DerivcoFootball.LeagueDataEndpoint do
   @moduledoc """
   """
@@ -62,6 +38,8 @@ defmodule DerivcoFootball.LeagueDataEndpoint do
   # internal handlers
   #
 
+  alias DerivcoFootball.ProtobufMessages
+  
   defp handle_get_json_league_season_pairs(conn) do
     send_resp(conn, 200, "/api/json/league_season_pairs here")
   end
